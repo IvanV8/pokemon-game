be6dceed47ea32e95e456e36d1caf20ec3e84468
IvanV8 <ibeloborodov@mail.ru>
2021-09-09 13:40:56 +0300

    2-е ДЗ

--- src/App.js
+++ src/App.js
@@ -1,22 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Header = ({title, desc}) => {
+  return (
+      <header className="root">
+        <div className="forest"></div>
+        <div className="container">
+          <h1>title</h1>
+          <p>desc</p>
+        </div>
+      </header>
+  )
+}
+
+const Layout = ({title,desc,urlBg, colorBg=0}) => {
+  return (
+      <section className="root">
+          <div className="wrapper">
+              <article>
+                  <div className="title">
+                      <h3>title</h3>
+                      <span className="separator"></span>
+                  </div>
+                  <div className="desc full">
+                      <p>desc</p>
+                  </div>
+              </article>
+          </div>
+      </section>
+  )
+}
+
+const Footer = () => {
+  return (
+      <footer>
+          <div className="wrapper">
+              <h3>THANKS FOR VISITING</h3>
+              <p>© 2021 #ReactMarathon.</p>
+          </div>
+      </footer>
+  )
+}
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Pokemon Game on React.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header title="This is pockemon game" desc="test"/>
+        <Layout title="H1" desc="" urlBg='./bg3.jpeg'/>
+        <Layout/>
+        <Layout/>
+        <Footer/>
+
       </header>
     </div>
   );
